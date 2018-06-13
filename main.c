d3307d6270e625334056b99e4d443cc65d4e5bfb
Diego Gervazoni <31870150+DiegoGervazoni@users.noreply.github.com>
2018-06-13 16:34:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,15 +10,14 @@
 
 int main()
 {
-    //maintest(); //uso para testesintegral
-    //maintestdois(); //teste zero
+
     setlocale(LC_ALL, "Portuguese");
 
-    int op;
+    char op;
     int opf;
-    double k,x;// variáveis usadas na opção Derivadas
-    double a,b; //variáveis usadas na opção Integral
-    double x0; //Variável usada na opção Zero da Função
+    double k,x;// variÃ¡veis usadas na opÃ§Ã£o Derivadas
+    double a,b; //variÃ¡veis usadas na opÃ§Ã£o Integral
+    double x0; //VariÃ¡vel usada na opÃ§Ã£o Zero da FunÃ§Ã£o
     double  result;
     double  *resultzero;
     char final;
@@ -26,41 +25,54 @@ int main()
  do{
  do{
  do{
- do{
-    printf("\n Escolha uma das opções abaixo: ");
+    printf("\n-------- CALCULADORA DE DERIVADAS, INTEGRAIS E ZERO DA FUNCAO ----------\n");
+    printf("\n Escolha uma das opÃ§Ãµes abaixo: ");
     printf("\n 1 - Derivadas");
     printf("\n 2 - Integral");
-    printf("\n 3 - Zero da função.");
-    printf("\n 4 - Sair\n");
+    printf("\n 3 - Zero da funÃ§Ã£o.");
+    printf("\n 4 - Sair (Encerrar o programa)\n");
     printf("\nEscolha a opcao: ");
-        scanf(" %d",&op);
+        scanf(" %c",&op);
             system("cls");
             fflush(stdin);
 
-printf("Opção inválida");
- }while(!(op == 1 || op == 2 || op == 3 || op == 4));
+{
 
+if (op > '4')
+{
+  printf("\nOpÃ§Ã£o invÃ¡lida. Digite um valor valido (numero) ou aperte 4 para encerrar o programa!\n");
+}
 
-        //verifica se um caractere é alfabético ou não
+}
+
+    {
+        //verifica se um caractere Ã© alfabÃ©tico ou nÃ£o
         if (isalpha(op))//se sim
 
         {
 
-            printf("\nVocê entrou com uma letra, por favor selecione um número\n");
+            printf("\nVocÃª entrou com uma letra, por favor digite um nÃºmero\n");
 
         }
 
+    }
+
+ }while(!(op == '1' || op == '2' || op == '3' || op == '4'));
+
+
                     {
-                        if (op == 4) {
-                            printf("-----------------Obrigado por utilizar nossa calculadora! -------------------");
+                        if (op == '4') {
+                            printf("\n-----------------Obrigado por utilizar nossa calculadora! -------------------\n");
                             exit(0);
                         };
                     }
 
  }while(isalpha(op));
 
+ if (op == '1' || op == '2')
+ {
   do{
-    printf("\n Escolha a Função abaixo: ");
+    printf("\n Escolha a FunÃ§Ã£o abaixo: ");
     printf("\n 1 - f(x) = k");
     printf("\n 2 - f(x) = x^k");
     printf("\n 3 - f(x) = k^x");
@@ -72,146 +84,67 @@ printf("Opção inválida");
     printf("\n 9 - f(x) = tg(x)");
     printf("\n 10 - f(x) = sqrt(x)");
     printf("\n 11 - f(x) = 1/x");
-    printf("\n 12 - Voltar ao menu opções.");
-    printf("\n 13 - Sair.");
+    printf("\n 12 - Voltar ao menu de escolha (Menu inicial ou sair).");
+    printf("\n 13 - Encerrar o programa.");
     printf("\n ");
     printf("\nEscolha sua opcao: ");
-        scanf("%d", &opf);
+        scanf(" %d", &opf);
             system("cls");
             fflush(stdin);
 
 
-                if (opf == 13){
-                        printf("-----------------Obrigado por utilizar nossa calculadora! -------------------");
-                        exit(0);
-                }
-
-
-
-printf("Opção inválida!");
- }while (opf > 13);
-
-        if (isalpha(opf))
-        {
-
-        printf("Você entrou com uma letra, por favor selecione um número");
-
-        }
-
- }while(isalpha(opf));
+                if (opf == 13)  {
+                                    printf("\n-----------------Obrigado por utilizar nossa calculadora! -------------------\n");
+                                    exit(0);
+                                }
 
 
 {
 
-        if(op == 1)
+    if (opf > 13)
+    {
+
+        printf("\nOpÃ§Ã£o invÃ¡lida. Digite um valor valido ou aperte 13 para sair!\n");
+    }
+
+}
+}while (opf > 13);
+
+ }
+
+if (op == '1')
+{
+
+
+{
+
+        if(opf == 4 || opf == 6 || opf == 7 || opf == 8 || opf == 9 || opf == 10 || opf == 11 || opf == 12)
     {
         printf("\n Digite o valor de X:\n ");
-            scanf("%lf", &x);
-        fflush(stdin);
+           scanf("%lf", &x);
+            fflush(stdin);
+                result = Derivada(x,opf,k);
+                    printf("\n Derivada = %.2lf \n", result);
+
     }
 
         if(opf == 1 || opf == 2 || opf == 3 || opf == 5)
         {
-        printf("\n Digite o valor de K:\n ");
-            scanf("% lf", &k);
-        fflush(stdin);
-        }
+        printf("\n Digite o valor de X:\n ");
+            scanf("%lf", &x);
+                fflush(stdin);
+                    printf("\n Digite o valor de K:\n ");
+                        scanf("%lf", &k);
+                            fflush(stdin);
+        result = Derivada(x,opf,k);
+        printf("\n Derivada = %.2lf \n", result);
 
-    if(opf == 1)
-        {
-            x = 0;
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-        if(opf == 2)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-        if(opf == 3)
-        {
-            //x = 0;
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %.2lf \n", result);
-        }
-
-
-        if(opf == 4)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-        if(opf == 5)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-        if(opf == 6)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-        if(opf == 7)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-        if(opf == 8)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-
-        if(opf == 9)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-
-        if(opf == 10)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
-        }
-
-
-
-        if(opf == 11)
-        {
-
-            result = Derivada(x,opf,k);
-            printf("\n Derivada = %lf \n", result);
         }
 
 }
+}
 
-
-    if(op == 2)
+    if(op == '2')
     {
 
         printf("\nDigite o valor de A: ");
@@ -222,34 +155,37 @@ printf("Opção inválida!");
             fflush(stdin);
 
         result = Integral(opf, k, a, b);
-        printf("\nIntegral = %lf\n", result);
+        printf("\nIntegral = %.2lf\n", result);
     }
 
-    if(op == 3)
+    if(op == '3')
     {
         printf("\nDigite o valor de X0 in R: ");
             scanf("%lf", &x0);
-            fflush(stdin);
+                fflush(stdin);
 
 
-        printf("\nEscolha a Função abaixo:\n");
+        printf("\nEscolha a FunÃ§Ã£o abaixo:\n");
         printf("\n 1 - f(x) = x^2 - 2");
         printf("\n 2 - f(x) = 2x - cos(x)");
         printf("\n 3 - f(x) = x . ln(x) - 1\n");
-            scanf("%c", &opf);
+        printf("\nSua escolha: ");
+            scanf("%d", &op);
+            fflush(stdin);
 
-    resultzero = Zero(x0,opf);
-    printf("\n Solução = %lf\n", resultzero);
-    printf("\n Erro = %lf\n", resultzero);
+    resultzero = Zero(x0,op);
+    printf("\n SoluÃ§Ã£o = %.2lf\n", resultzero);
+    printf("\n Erro = %.2lf\n", resultzero);
     }
 
-    printf("\nDeseja efetuar outro calculo?\n");
-    printf("\nS - Voltar ao menu\n");
-    printf("\nN - Sair \n");
+    printf("\nO que deseja fazer?\n");
+    printf("\nS - Para voltar ao menu inicial\n");
+    printf("\nN - Para sair \n");
     printf("\nEscolha a opcao: ");
     fflush(stdin);
     final = getchar();
     system("cls");
+    system("PAUSE");
 
   }while(final == 'N' || final == 'n' && final == 'S' || final == 's');
 
